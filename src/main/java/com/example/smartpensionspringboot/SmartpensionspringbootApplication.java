16d788a1671958329c78b710ceaca6d2fe503b4f
lanyankongjian <1749260669@qq.com>
2021-04-01 21:26:55 +0800

    Merge branch 'master' of https://github.com/lanyankongjian/smartpensionspringboot

---
+++ src/main/java/com/example/smartpensionspringboot/SmartpensionspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartpensionspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartpensionspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartpensionspringbootApplication.class, args);
+    }
+
+}
