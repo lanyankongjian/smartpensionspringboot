52fbd0e9b65d6777bf1bf6506ab266e46c8cf14b
lanyankongjian <1749260669@qq.com>
2021-04-03 09:30:51 +0800

    后端框架

---
+++ src/test/java/com/example/smartpensionspringboot/SmartpensionspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartpensionspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartpensionspringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
