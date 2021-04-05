cae007f8fc9ce5fe5b76305c398a375c5fa74796
lanyankongjian <1749260669@qq.com>
2021-04-05 19:40:21 +0800

    提交

---
+++ src/main/java/com/example/smartpensionspringboot/cj/pojos/AssessmentRegistration.java
@@ -0,0 +1,101 @@
+package com.example.smartpensionspringboot.cj.pojos;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.util.Date;
+
+//评估管理--评估登记
+public class AssessmentRegistration {
+    private Integer arId;
+    private Integer oldName;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date updateTime;
+    @JsonFormat(pattern = "yyyy-MM-dd",timezone = "GMT+8")
+    private Date evaluationDate;
+    private String assessmentType;
+    private String assessor;
+    private String assessmentLocation;
+    private Date registrationTime;
+    private String registrant;
+    private String assessmentResults;
+
+    public Integer getArId() {
+        return arId;
+    }
+
+    public void setArId(Integer arId) {
+        this.arId = arId;
+    }
+
+    public Integer getOldName() {
+        return oldName;
+    }
+
+    public void setOldName(Integer oldName) {
+        this.oldName = oldName;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Date getEvaluationDate() {
+        return evaluationDate;
+    }
+
+    public void setEvaluationDate(Date evaluationDate) {
+        this.evaluationDate = evaluationDate;
+    }
+
+    public String getAssessmentType() {
+        return assessmentType;
+    }
+
+    public void setAssessmentType(String assessmentType) {
+        this.assessmentType = assessmentType;
+    }
+
+    public String getAssessor() {
+        return assessor;
+    }
+
+    public void setAssessor(String assessor) {
+        this.assessor = assessor;
+    }
+
+    public String getAssessmentLocation() {
+        return assessmentLocation;
+    }
+
+    public void setAssessmentLocation(String assessmentLocation) {
+        this.assessmentLocation = assessmentLocation;
+    }
+
+    public Date getRegistrationTime() {
+        return registrationTime;
+    }
+
+    public void setRegistrationTime(Date registrationTime) {
+        this.registrationTime = registrationTime;
+    }
+
+    public String getRegistrant() {
+        return registrant;
+    }
+
+    public void setRegistrant(String registrant) {
+        this.registrant = registrant;
+    }
+
+    public String getAssessmentResults() {
+        return assessmentResults;
+    }
+
+    public void setAssessmentResults(String assessmentResults) {
+        this.assessmentResults = assessmentResults;
+    }
+}
---
+++ src/main/java/com/example/smartpensionspringboot/cj/pojos/MedicalRegistration.java
@@ -0,0 +1,83 @@
+package com.example.smartpensionspringboot.cj.pojos;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.util.Date;
+
+//健康管理--就医登记
+public class MedicalRegistration {
+    private Integer mrId;
+    private Integer oldName;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date updateTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date medicalTime;
+    private String physician;
+    private String placeOfMedical;
+    private String diagnosisResults;
+    private String medicalAdvice;
+
+    public Integer getMrId() {
+        return mrId;
+    }
+
+    public void setMrId(Integer mrId) {
+        this.mrId = mrId;
+    }
+
+    public Integer getOldName() {
+        return oldName;
+    }
+
+    public void setOldName(Integer oldName) {
+        this.oldName = oldName;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Date getMedicalTime() {
+        return medicalTime;
+    }
+
+    public void setMedicalTime(Date medicalTime) {
+        this.medicalTime = medicalTime;
+    }
+
+    public String getPhysician() {
+        return physician;
+    }
+
+    public void setPhysician(String physician) {
+        this.physician = physician;
+    }
+
+    public String getPlaceOfMedical() {
+        return placeOfMedical;
+    }
+
+    public void setPlaceOfMedical(String placeOfMedical) {
+        this.placeOfMedical = placeOfMedical;
+    }
+
+    public String getDiagnosisResults() {
+        return diagnosisResults;
+    }
+
+    public void setDiagnosisResults(String diagnosisResults) {
+        this.diagnosisResults = diagnosisResults;
+    }
+
+    public String getMedicalAdvice() {
+        return medicalAdvice;
+    }
+
+    public void setMedicalAdvice(String medicalAdvice) {
+        this.medicalAdvice = medicalAdvice;
+    }
+}
